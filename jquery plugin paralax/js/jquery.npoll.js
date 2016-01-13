7b44e61684f0c14881910b1ee6fc8265dcf7b6f6
Den-web <kirevden@gmail.com>
2016-01-13 21:10:44 +0200

    Parallax Effect for web page

---
+++ jquery plugin paralax/js/jquery.npoll.js
@@ -0,0 +1,33 @@
+;(function($){
+	$.fn.npoll = function(){
+		
+		$('<h1/>',{
+		  text: "Какая ваша любимая JavaScript библиотека?"
+		  }).appendTo(this);
+		  
+		  var form = $('<form/>').appendTo(this);
+		  
+		  var x, y, categories = ["jQuery", "YUI", "Dojo", "ExtJS", "Zepto"];
+		  
+		  for(x = 0, y -categories.length; x < y; x++){
+			
+			$('<input/>',{
+				type: "radio",
+				name: "categories",
+				id: categories[x],
+				value: categories[x]
+			}).appendTo(form);
+			
+			$('<label/>',{
+				text: categories[x],
+				"for": categories[x]
+			}).appendTo(form);
+			
+		  }
+		  
+		  $('<button/>',{
+			text: "Ответить!"
+		  }).appendTo(this);
+		return (this); 
+	};
+})(jQuery);
