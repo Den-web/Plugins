fcae53b27b3f03dd2eb14ec6fc867761a70d7cad
Den-web <kirevden@gmail.com>
2016-02-05 22:23:04 +0200

    Еффект гармошки

---
+++ responsive_3dfoldscroll-master/index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<!--[if IEMobile 7]><html class="iem7" lang="en" dir="ltr"><![endif]-->
+<!--[if lte IE 6]><html class="lt-ie9 lt-ie8 lt-ie7" lang="en" dir="ltr"><![endif]-->
+<!--[if (IE 7)&(!IEMobile)]><html class="lt-ie9 lt-ie8" lang="en" dir="ltr"><![endif]-->
+<!--[if IE 8]><html class="lt-ie9" lang="en" dir="ltr"><![endif]-->
+
+<html>
+	<head>
+		<link href="includes/css/global.css" rel="stylesheet" type="text/css">
+	    <title>Создаем эффект "гармошки" для страницы сайта | Демонстрация для сайта RUDEBOX</title>
+		<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+		<script src="includes/js/responsive_3dfoldscroll.js?v=1"></script>
+	</head>
+	<body>
+	<!-- Используется только для демонстрации -->
+<div id="controls">
+<div align=right><a class="close" title="Закрыть" onclick="document.getElementById('controls').style.display='none';">X</a></div>
+   <!-- Яндекс.Директ -->
+<div id="yandex_ad"></div>
+<script type="text/javascript">
+(function(w, d, n, s, t) {
+    w[n] = w[n] || [];
+    w[n].push(function() {
+        Ya.Direct.insertInto(84098, "yandex_ad", {
+            ad_format: "direct",
+            type: "240x400",
+            border_type: "block",
+            border_radius: true,
+            site_bg_color: "FFFFFF",
+            header_bg_color: "FEEAC7",
+            bg_color: "FFF9F0",
+            border_color: "FBE5C0",
+            title_color: "0000CC",
+            url_color: "006600",
+            text_color: "000000",
+            hover_color: "0066FF",
+            favicon: true,
+            no_sitelinks: false
+        });
+    });
+    t = d.getElementsByTagName("script")[0];
+    s = d.createElement("script");
+    s.src = "//an.yandex.ru/system/context.js";
+    s.type = "text/javascript";
+    s.async = true;
+    t.parentNode.insertBefore(s, t);
+})(window, document, "yandex_context_callbacks");
+</script>
+</div>
+<table cellpadding="0" cellspacing="0" id="demo_navi">
+  <tr>
+    <td width="220"><a href="/ " rel="nofollow" ><div class="logo"><b><font color=#c3e425>RUDE</font>BOX</b></div></a></td><td width="45"><div class="demo">DEMO</div></td>
+	<td style="text-overflow: ellipsis;"><a href="/responsive_3dfoldscroll-master" class="go-bottom" rel="nofollow" ><div class="name">Создаем эффект "гармошки" для страницы сайта</div></a></td>
+  </tr>
+</table>
+	
+		<div id="page">
+			<div id="container" style="">
+				<div class="foldWrapper clearfix">
+					<img src="includes/images/p1.jpg" />
+				</div>
+				<div class="foldWrapper clearfix">
+					<img src="includes/images/p2.jpg" />
+				</div>
+				<div class="foldWrapper clearfix">
+					<img src="includes/images/p3.jpg" />
+				</div>
+				<div class="foldWrapper clearfix">
+					<img src="includes/images/p1.jpg" />
+				</div>
+				<div class="foldWrapper clearfix">
+					<img src="includes/images/p2.jpg" />
+				</div>
+				<div class="foldWrapper clearfix">
+					<img src="includes/images/p3.jpg" />
+				</div>
+				<div class="foldWrapper clearfix">
+					<img src="includes/images/p1.jpg" />
+				</div>
+				<div class="foldWrapper clearfix">
+					<img src="includes/images/p2.jpg" />
+				</div>
+				<div class="foldWrapper clearfix">
+					<img src="includes/images/p3.jpg" />
+				</div>
+			</div>
+			<div id="footer-top">
+
+			</div>
+		</div>
+		
+
+        <link media="screen" href="demo/styles/demo.css" type="text/css" rel="stylesheet" />	
+		
+		
+		
+		<script>
+			$( document ).ready(function() {
+				window.onload=function(){
+					var fold = $.fn.responsive_3dfoldscroll();
+				};
+			});
+		
+		</script>
+		
+	</body>
+</html>
